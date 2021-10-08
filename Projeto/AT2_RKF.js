0e3948b788ef318f8a2a02fcca9d304a7043523b
xLeafarx <rafael.k.faria@gmail.com>
2021-10-07 22:25:58 -0300

    Aula 2 - RKF

---
+++ Projeto/AT2_RKF.js
@@ -0,0 +1,11 @@
+// Atividade aula 2 - RKF
+
+var valor1 = 5;
+var valor2 = 2;
+var valor3 = 4;
+var valor4 = 10;
+var total;
+
+total = ((valor1 + valor2) / valor3) * valor4;
+
+console.log(total);
